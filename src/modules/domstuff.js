ab46ec2d687d7b539f62dcd6ec9ef1783cdcc8c7
Edmund Debrah <edmundking05@gmail.com>
2023-01-25 16:29:30 +0000

    add function addProjToDom

--- src/modules/domstuff.js
+++ src/modules/domstuff.js
@@ -1,1 +1,19 @@
 // add dom manipulation logic
+const tasklist = document.getElementById('tasklist');
+const projectBtns = document.querySelectorAll('#projectBtn');
+const addProjectBtn = document.getElementById('addProjectBtn');
+let addingProj = false;
+
+// add behaviour for when project button is clicked
+
+// add behaviour when addProject is clicked
+const projectSection = document.getElementById('userProjects');
+function addProjToDom() {
+  const projectPrompt = document.createElement('div');
+  projectPrompt.innerHTML = `<input type="text" value="" placeholder="project title" data-project-title>
+    <button id="add" style="background-color: rgb(123, 237, 157);">Add</button>
+    <button id="cancel" style="background-color: rgb(247, 95, 95)">Cancel</button>`;
+  projectSection.append(projectPrompt);
+  addProjectBtn.style.display = 'none';
+  addingProj = true;
+}
