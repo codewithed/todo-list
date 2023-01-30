1fd136325e7d72ec9c2f409c92a504d71e9a9260
Edmund Debrah <edmundking05@gmail.com>
2023-01-30 16:57:14 +0000

    update domstuff.js

--- src/modules/domstuff.js
+++ src/modules/domstuff.js
@@ -4,24 +4,21 @@ import Project from './project';
 import Storage from './storage';
 
 export default function loadUi() {
-  // add dom manipulation logic
-  const tasklist = document.getElementById('tasklist');
-  const projectBtns = document.querySelectorAll('#projectBtn');
-  const addProjectBtn = document.getElementById('addProjectBtn');
-  let addingProj = false;
+  // create basic needed objects
+  const defaultProject = Project('defaultProject');
+  const currentProject = defaultProject;
+  const projectList = TodoList();
 
-  // add behaviour for when project button is clicked
+  // creates project
+  function createProject(projectName) {
+    let name = projectName;
+    name = Project(projectName);
+    projectList.addProject(name);
+  }
 
-  // add behaviour when addProject is clicked
-  const projectSection = document.getElementById('userProjects');
-  function addProjToDom() {
-    const projectPrompt = document.createElement('div');
-    projectPrompt.classList.add('project-prompt');
-    projectPrompt.innerHTML = `<input type="text" value="" placeholder="project title" data-project-title>
-  <div><button id="add" style="background-color: rgb(123, 237, 157);">Add</button>
-<button id="cancel" style="background-color: rgb(247, 95, 95)">Cancel</button></div>`;
-    projectSection.appendChild(projectPrompt);
-    addProjectBtn.style.display = 'none';
-    addingProj = true;
+  // adds task to current project
+  function addTask(title, dueDate, description, priority, completedStatus) {
+    const task = Todo(title, dueDate, description, priority, completedStatus);
+    currentProject.addTask(task);
   }
 }
