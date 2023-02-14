b273173b2764a010dd71f43d1eef97c7642a597c
Edmund Debrah <edmundking05@gmail.com>
2023-02-14 19:20:28 +0000

    update storage.js

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,1 +1,14 @@
-// storage logic goes here
+// Saves an Item to Local Storage
+export function saveItem(itemName, itemObject) {
+  localStorage.setItem(itemName, JSON.stringify(itemObject));
+}
+
+// Gets an Item from local storage
+export function getItem(itemName) {
+  return (localStorage.getItem(itemName));
+}
+
+// removes an Item from local storage
+export function deleteItem(itemName) {
+  localStorage.removeItem(itemName);
+}
