7d9e514dde6fff357a2c38ab35e0b9a0690eb539
Edmund Debrah <edmundking05@gmail.com>
2023-02-15 12:25:27 +0000

    clean code

--- src/modules/domstuff.js
+++ src/modules/domstuff.js
@@ -9,6 +9,130 @@ export default function loadUi() {
   let currentProject = defaultProject;
   const projectList = new ProjectList();
 
+  // creates new project from projectPrompt
+  function addProject(title) {
+    const newProj = new Project(title);
+    projectList.addProject(newProj);
+  }
+
+  // deletes a project
+  function deleteProject(e) {
+    if (e.target.classList.contains('fa-xmark')) {
+      const projectBtn = e.target.parentElement.parentElement;
+      const num = projectBtn.dataset.index;
+      projectList.removeProject(num);
+      projectBtn.remove();
+      e.stopPropagation();
+    }
+  }
+
+  // updates a todo item
+  function updateTodo(e) {
+    if (e.target.classList.contains('todo-title') || e.target.classList.contains('due-date')) {
+      const value = e.target;
+      value.style.display = 'none';
+
+      // make input element visible
+      const inputItem = e.target.parentElement.querySelector('input');
+      inputItem.classList.add('active');
+
+      // listen for enter event
+      document.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+          const input = inputItem.value;
+          if (input === '' || input === undefined) {
+            alert('Value cannot be empty');
+          } else {
+            inputItem.classList.remove('active');
+            value.style.display = 'block';
+            const index = value.parentElement.parentElement.dataset;
+            value.innerText = input;
+
+            // update todo item with new data
+            if (value.classList.contains('todo-title')) {
+              currentProject.arr[index].editTitle(input);
+            }
+            if (value.classList.contains('due-date')) {
+              currentProject.arr[index].editDueDate(input);
+            }
+          }
+        }
+      });
+    }
+  }
+
+  // deletes todo task
+  function deleteTodo(e) {
+    // remove todo component
+    if (e.target.classList.contains('fa-xmark')) {
+      const todo = e.target.parentElement.parentElement;
+      todo.remove();
+      // remove the corresponding todo object
+      const num = todo.dataset.index;
+      currentProject.removeTask(num);
+    }
+  }
+
+  // create todo component
+  function createTodoComponent(title, date) {
+    const todoSection = document.getElementById('tasklist');
+    const todoItem = document.createElement('button');
+    todoItem.classList.add('todo-item');
+    todoItem.innerHTML = `<div class="left-task-panel"><i class="fa-regular fa-circle"></i><p class="todo-title">${title}</p><input type="text" class="input-task-name" data-input-task-name=""></div>
+    <div class="right-task-panel"><p class='due-date'>${date}</p><input type="date" name="" id="inputDueDate" class="input-date" data-input-date><i class="fa-solid fa-xmark"></i></i></div>`;
+    const index = currentProject.arr.length;
+    todoItem.setAttribute('data-index', index);
+    todoSection.append(todoItem);
+    todoItem.addEventListener('click', deleteTodo);
+    todoItem.addEventListener('click', updateTodo);
+  }
+
+  // loads todos of a project
+  function loadTodos(arr) {
+    arr.forEach((element) => {
+      createTodoComponent(element.title, element.dueDate);
+    });
+  }
+
+  // shows current project title in the todo section
+  function showProjInDom(projectName) {
+    const todoSection = document.getElementById('tasklist');
+    todoSection.innerHTML = '';
+    const projtitle = document.getElementById('projectTitle');
+    projtitle.innerText = projectName;
+  }
+
+  function createProjectButton(title) {
+    // create project button
+    const userProjects = document.getElementById('userProjects');
+    const projectButton = document.createElement('button');
+    projectButton.classList.add('project-button');
+    projectButton.style.id = 'projectButton';
+    const projId = projectList.arr.length - 1;
+    projectButton.setAttribute('data-index', projId);
+    projectButton.innerHTML = `<div class="left-project-panel"><i class="fa-solid fa-list"></i><span>&nbsp${title}</span></div>
+     <div class="right-project-panel"><i class="fa-solid fa-xmark"></i></div>`;
+    userProjects.appendChild(projectButton);
+    const addProjectBtn = document.getElementById('addProjectBtn');
+    addProjectBtn.style.display = 'block';
+
+    // add event listeners
+    projectButton.addEventListener('click', (e) => {
+      deleteProject(e);
+      currentProject = projectList.arr[projId];
+      showProjInDom(currentProject.name);
+      loadTodos(currentProject.arr);
+    });
+  }
+
+  // removes project prompt
+  function removeProjectPrompt() {
+    const addProjectBtn = document.getElementById('addProjectBtn');
+    const projectPrompt = document.getElementById('projectPrompt');
+    projectPrompt.remove();
+    addProjectBtn.style.display = 'block';
+  }
+
   // creates projectPrompt and appends it to screen
   function promptProject() {
     const projectPrompt = document.createElement('div');
@@ -38,91 +162,32 @@ export default function loadUi() {
     cancel.addEventListener('click', removeProjectPrompt);
   }
 
-  // shows current project title in the todo section
-  function showProjInDom(projectName) {
-    const todoSection = document.getElementById('tasklist');
-    todoSection.innerHTML = '';
-    const projtitle = document.getElementById('projectTitle');
-    projtitle.innerText = projectName;
+  // ADD FUNCTIONS FOR GETTING TODO ITEMS
+
+  function removeTodoPrompt() {
+    const todoPrompt = document.getElementById('todoPrompt');
+    todoPrompt.remove();
+    const addTodoBtn = document.getElementById('addTodoBtn');
+    addTodoBtn.style.display = 'block';
   }
 
-  // deletes a project
-  function deleteProject(e) {
-    if (e.target.classList.contains('fa-xmark')) {
-      const projectBtn = e.target.parentElement.parentElement;
-      const num = projectBtn.dataset.index;
-      projectList.removeProject(num);
-      projectBtn.remove();
-      e.stopPropagation();
+  // add a todo task to the current project
+  function addTodo() {
+    const todoPrompt = document.getElementById('todoPrompt');
+    const taskName = document.getElementById('taskTitle').value;
+    if (taskName === undefined || taskName === '' || taskName === null) {
+      alert('Task name cannot be empty');
+    } else {
+      // create todo object
+      const todo = Todo(taskName);
+      currentProject.addTask(todo);
+      todoPrompt.remove();
 
-      /*
-      if (getItem('defaultProject') !== null || getItem('defaultProject') !== undefined) {
-        defaultProject = JSON.parse(localStorage.defaultProject);
-      }
-      */
-
-      currentProject = defaultProject;
-
-      showProjInDom('Inbox');
-      loadTodos(currentProject.arr);
+      createTodoComponent(taskName);
+      const addTodoBtn = document.getElementById('addTodoBtn');
+      addTodoBtn.style.display = 'block';
     }
   }
-
-  // loads todos of a project
-  function loadTodos(arr) {
-    arr.forEach((element) => {
-      // create todo component
-      const todoSection = document.getElementById('tasklist');
-      const todoItem = document.createElement('button');
-      todoItem.classList.add('todo-item');
-      todoItem.innerHTML = `<div class="left-task-panel"><i class="fa-regular fa-circle"></i><p class="todo-title">${element.title}</p><input type="text" class="input-task-name" data-input-task-name=""></div>
-      <div class="right-task-panel"><p class='due-date'>${element.dueDate}</p><input type="date" name="" id="inputDueDate" class="input-date" data-input-date><i class="fa-solid fa-xmark"></i></i></div>`;
-      const index = currentProject.arr.length;
-      todoItem.setAttribute('data-index', index);
-      todoSection.append(todoItem);
-      todoItem.addEventListener('click', deleteTodo);
-    });
-  }
-
-  // creates new project from projectPrompt
-  function addProject(title) {
-    const newProj = new Project(title);
-    projectList.addProject(newProj);
-  }
-
-  function createProjectButton(title) {
-    // create project button
-    const userProjects = document.getElementById('userProjects');
-    const projectButton = document.createElement('button');
-    projectButton.classList.add('project-button');
-    projectButton.style.id = 'projectButton';
-    const projId = projectList.arr.length - 1;
-    projectButton.setAttribute('data-index', projId);
-    projectButton.innerHTML = `<div class="left-project-panel"><i class="fa-solid fa-list"></i><span>&nbsp${title}</span></div>
-     <div class="right-project-panel"><i class="fa-solid fa-xmark"></i></div>`;
-    userProjects.appendChild(projectButton);
-    const addProjectBtn = document.getElementById('addProjectBtn');
-    addProjectBtn.style.display = 'block';
-
-    // add event listeners
-    projectButton.addEventListener('click', () => {
-      currentProject = projectList.arr[projId];
-      showProjInDom(currentProject.name);
-      loadTodos(currentProject.arr);
-    });
-
-    projectButton.addEventListener('click', (e) => { deleteProject(e); });
-  }
-
-  // removes project prompt
-  function removeProjectPrompt() {
-    const addProjectBtn = document.getElementById('addProjectBtn');
-    const projectPrompt = document.getElementById('projectPrompt');
-    projectPrompt.remove();
-    addProjectBtn.style.display = 'block';
-  }
-
-  // ADD FUNCTIONS FOR GETTING TODO ITEMS
 
   function promptTodo() {
     // create todo prompt component
@@ -142,89 +207,6 @@ export default function loadUi() {
 
     add.addEventListener('click', addTodo);
     cancel.addEventListener('click', removeTodoPrompt);
-  }
-
-  // deletes todo task
-  function deleteTodo(e) {
-    // remove todo component
-    if (e.target.classList.contains('fa-xmark')) {
-      const todo = e.target.parentElement.parentElement;
-      todo.remove();
-      // remove the corresponding todo object
-      const num = todo.dataset.index;
-      currentProject.removeTask(num);
-      // deleteItem(todo.title);
-    }
-  }
-
-  // updates a todo item
-  function updateTodo(e) {
-    if (e.target.classList.contains('todo-title') || e.target.classList.contains('due-date')) {
-      const value = e.target;
-      value.style.display = 'none';
-
-      // make input element visible
-      const inputItem = e.target.parentElement.querySelector('input');
-      inputItem.classList.add('active');
-
-      // listen for enter event
-      document.addEventListener('keyup', (event) => {
-        if (event.key === 'Enter') {
-          const input = inputItem.value;
-          if (input === '' || value === undefined) {
-            alert('Value cannot be empty');
-          } else {
-            inputItem.classList.remove('active');
-            value.style.display = 'block';
-            const index = value.parentElement.parentElement.dataset;
-            value.innerText = input;
-
-            // update todo item with new data
-            if (value.classList.contains('todo-title')) {
-              currentProject.arr[index].editTitle(input);
-            }
-            if (value.classList.contains('due-date')) {
-              currentProject.arr[index].editDueDate(input);
-            }
-          }
-        }
-      });
-    }
-  }
-
-  // add a todo task to the current project
-  function addTodo() {
-    const todoPrompt = document.getElementById('todoPrompt');
-    const taskName = document.getElementById('taskTitle').value;
-    if (taskName === undefined || taskName === '' || taskName === null) {
-      alert('Task name cannot be empty');
-    } else {
-      // create todo object
-      const todo = Todo(taskName);
-      currentProject.addTask(todo);
-      todoPrompt.remove();
-
-      // create todo component
-      const todoItem = document.createElement('button');
-      todoItem.classList.add('todo-item');
-      todoItem.innerHTML = `<div class="left-task-panel"><i class="fa-regular fa-circle"></i><p class="todo-title">${taskName}</p><input type="text" class="input-task-name" data-input-task-name=""></div>
-      <div class="right-task-panel"><p class="due-date">No Date</p><input type="date" name="" id="inputDueDate" class="input-date" data-input-date><i class="fa-solid fa-xmark"></i></i></div>`;
-      const todoSection = document.getElementById('tasklist');
-      const index = currentProject.arr.length - 1;
-      todoItem.setAttribute('data-index', index);
-      todoSection.append(todoItem);
-      todoItem.addEventListener('click', deleteTodo);
-      todoItem.addEventListener('click', updateTodo);
-      const addTodoBtn = document.getElementById('addTodoBtn');
-      addTodoBtn.style.display = 'block';
-    }
-  }
-
-  function removeTodoPrompt() {
-    const todoPrompt = document.getElementById('todoPrompt');
-    todoPrompt.remove();
-    const addTodoBtn = document.getElementById('addTodoBtn');
-    addTodoBtn.style.display = 'block';
   }
 
   // get the todo tasks in inbox
